f1e6319681b9136296227004c68a924a069df5e3
elv-wayne <38925608+elv-wayne@users.noreply.github.com>
2022-08-25 21:32:55 -0400

    Offer signature fix (#74)
    * Add new ElvTradable contracts Redeemable
    
    * Update ElvTradable contracts for Redeemable
    
    * Add account_offer_signature
    
    * Add elv-live add_offer command
    
    * add nft_remove_offer
    
    * Add nft_offer_active and nft_offer_redeemed
    
    * Add packedData to the output of account_offer_signature
    
    * Fix CreateOfferSignature to use arrify before signing
    
    Co-authored-by: elv-wayne <wayne@eluv.io>

--- src/ElvAccount.js
+++ src/ElvAccount.js
@@ -227,16 +227,20 @@ class ElvAccount {
     const mintAddressBytes = ethers.utils.arrayify(mintHelperAddress);
     const tokenIdBigInt = ethers.BigNumber.from(tokenId).toHexString();
 
-    const message = ethers.utils.keccak256(
-      ethers.utils.solidityPack(
-        ["bytes", "bytes", "uint256", "uint8"],
-        [nftAddressBytes, mintAddressBytes, tokenIdBigInt, offerId]
-      )
+    const packedData = ethers.utils.solidityPack(
+      ["bytes", "bytes", "uint256", "uint8"],
+      [nftAddressBytes, mintAddressBytes, tokenIdBigInt, offerId]
     );
 
-    const signature = await this.client.signer.signMessage(message);
-    const components = ethers.utils.splitSignature(signature);
-    return {message, signature, components};
+    const encodedData = ethers.utils.keccak256(
+      packedData
+    );
+
+    let messageHashBytes = ethers.utils.arrayify(encodedData);
+
+    const signedData = await this.client.signer.signMessage(messageHashBytes);
+    const signature = ethers.utils.splitSignature(signedData);
+    return {encodedData, packedData, signedData, signature};
   }
 
 }
