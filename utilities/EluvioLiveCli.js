04f915abf6f82a7944abe3035d9c998179ace5e3
Todd Hodes <108554594+elv-todd@users.noreply.github.com>
2024-01-09 15:26:10 -0800

    fix offerId output on-add-redeemable-offer (#160)

--- utilities/EluvioLiveCli.js
+++ utilities/EluvioLiveCli.js
@@ -1110,7 +1110,7 @@ const CmdNftAddRedeemableOffer = async ({ argv }) => {
     res = await elvlv.NFTAddRedeemableOffer({ addr: argv.addr });
 
     console.log(yaml.dump(res));
-    console.log("Offer ID: ",res.logs[0].values.offerId);
+    console.log("added offerId", res?.logs[0]?.args[0] ?? "unknown offerId");
   } catch (e) {
     console.error("ERROR:", e);
   }
