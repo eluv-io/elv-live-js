4ea7ccf92937cb06ef428cecc627912ea765fc8c
elv-preethi <preethi.manoharan@eluv.io>
2025-02-20 16:12:18 -0800

    make changes as per comments

--- scripts/set_library_tenant.sh
+++ scripts/set_library_tenant.sh
@@ -5,33 +5,36 @@ set -Eeuo pipefail
 # help function used when input parameters are empty
 help_function()
 {
-    echo ""
-    echo "Usage : ./set_library_tenant <owner_key> <comma separated libraries list> <tenant_contract_id>"
+    echo "export PRIVATE_KEY='<owner_key>'"
+    echo "Usage : ./set_library_tenant <comma separated libraries list> <tenant_contract_id>"
     exit 0 # Exit script after printing help
 }
 
-if [[ $# -lt 3 ]]; then
+if [[ $# -lt 2 ]]; then
   help_function
 fi
 
-private_key=$1
-libraries=$2
-tenant_contract_id=$3
+libraries=$1
+tenant_contract_id=$2
 
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 elv_live_js_dir="$(dirname "$base_dir")";
 cmd="$elv_live_js_dir/elv-admin"
 
-export PRIVATE_KEY=${private_key}
+if ! env | grep -q "^PRIVATE_KEY="; then
+  echo "Error: PRIVATE_KEY is not exported"
+  exit 1
+fi
+
 IFS="," read -r -a lib_arr <<< "$libraries";
 for elem in "${lib_arr[@]}"; do
-  out="$(${cmd} set_tenant_contract_id "$elem" "$tenant_contract_id")"
-  if [[ $? -eq 0 ]]; then
-    echo "Success: tenant_contract_id set on ${elem}"
-  else
+  out="$(${cmd} set_tenant_contract_id "$elem" "$tenant_contract_id")";
+  if [[ $out == *"error"* ]]; then
     echo "Failed: to set tenant_contract_id on ${elem}"
     echo "$out"
     exit 1
+  else
+    echo "Success: tenant_contract_id set on ${elem}"
   fi
 done
 
