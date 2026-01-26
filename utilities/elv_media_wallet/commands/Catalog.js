60ebf669af398cec65bbb639d53b85fe72797bb6
elv-preethi <preethi.manoharan@eluv.io>
2026-01-26 14:56:51 -0800

    fix indent

--- utilities/elv_media_wallet/commands/Catalog.js
+++ utilities/elv_media_wallet/commands/Catalog.js
@@ -1,121 +1,121 @@
 const CatalogCommand = require("../lib/Catalog");
 
 module.exports = {
-    command: "catalog <command>",
-    describe: "Catalog related commands",
-    builder: (yargs) => {
-        yargs
+  command: "catalog <command>",
+  describe: "Catalog related commands",
+  builder: (yargs) => {
+    yargs
+      .command(
+        "list <object_id>",
+        "List catalog items",
+        (yargs) => {
+          yargs
+            .positional("object_id", {
+              describe: "Catalog Object ID",
+              type: "string",
+            });
+        },
+        (argv) => {
+          CatalogCommand.CmdCatalogList({argv});
+        }        
+      )
+      .command(
+        "item <command>",
+        "Catalog item related commands",
+        (yargs) => {
+          yargs
             .command(
-                "list <object_id>",
-                "List catalog items",
-                (yargs) => {
-                    yargs
-                        .positional("object_id", {
-                            describe: "Catalog Object ID",
-                            type: "string",
-                        })
-                },
-                (argv) => {
-                    CatalogCommand.CmdCatalogList({argv});
-                }        
+              "get <object_id> <item_id>",
+              "Get catalog item details",
+              (yargs) => {
+                yargs
+                  .positional("object_id", {
+                    describe: "Catalog Object ID",
+                    type: "string",
+                  })
+                  .positional("item_id", {
+                    describe: "Catalog Item ID",
+                    type: "string",
+                  });
+              },
+              (argv) => {
+                CatalogCommand.CmdCatalogItemGet({argv});
+              }        
             )
             .command(
-                "item <command>",
-                "Catalog item related commands",
-                (yargs) => {
-                    yargs
-                        .command(
-                            "get <object_id> <item_id>",
-                            "Get catalog item details",
-                            (yargs) => {
-                                yargs
-                                    .positional("object_id", {
-                                        describe: "Catalog Object ID",
-                                        type: "string",
-                                    })
-                                    .positional("item_id", {
-                                        describe: "Catalog Item ID",
-                                        type: "string",
-                                    })
-                            },
-                            (argv) => {
-                                CatalogCommand.CmdCatalogItemGet({argv});
-                            }        
-                        )
-                        .command(
-                            "set <object_id> <item_id> [options]",
-                            "Set catalog item content",
-                            (yargs) => {
-                                yargs
-                                    .positional("object_id", {
-                                        describe: "Catalog Object ID",
-                                        type: "string",
-                                    })
-                                    .positional("item_id", {
-                                        describe: "Catalog Item ID",
-                                        type: "string",
-                                    })
-                                    .option("content_id", {
-                                        alias: "c",
-                                        describe: "Content ID to set for the catalog item",
-                                        type: "string",
-                                    })
-                                    .option("content_id_type", {
-                                        alias: "t",
-                                        describe: "Type of Content ID",
-                                        type: "string",
-                                    })
-                                    .option("public", {
-                                        alias: "p",
-                                        describe: "Set Item to Public",
-                                        type: "boolean",
-                                    })
-                                    .option("composition_key", {
-                                        alias: "ck",
-                                        describe: "Name of Composition Key",
-                                        type: "string"
-                                    })
-                            },
-                            (argv) => {
-                                CatalogCommand.CmdCatalogItemSet({argv});
-                            }        
-                        )
-                        .command(
-                            "add <object_id> [options]",
-                            "Add object as catalog item",
-                            (yargs) => {
-                                yargs
-                                    .positional("object_id", {
-                                        describe: "Catalog Object ID",
-                                        type: "string",
-                                    })
-                                    .option("item_name", {
-                                        alias: "n", 
-                                        describe: "New Media Item Name",
-                                        type: "string"
-                                    })
-                                    .option("content_id", {
-                                        alias: "c",
-                                        describe: "Content ID to add as catalog item",
-                                        type: "string",
-                                    })
-                                    .option("content_id_type", {
-                                        alias: "t",
-                                        describe: "Type of Content ID",
-                                        type: "string",
-                                    })
-                                    .option("public", {
-                                        alias: "p",
-                                        describe: "Set Item to Public",
-                                        type: "boolean",
-                                        default: false
-                                    })
-                            },
-                            (argv) => {
-                                CatalogCommand.CmdCatalogItemAdd({argv});
-                            }        
-                        )
-                }        
+              "set <object_id> <item_id> [options]",
+              "Set catalog item content",
+              (yargs) => {
+                yargs
+                  .positional("object_id", {
+                    describe: "Catalog Object ID",
+                    type: "string",
+                  })
+                  .positional("item_id", {
+                    describe: "Catalog Item ID",
+                    type: "string",
+                  })
+                  .option("content_id", {
+                    alias: "c",
+                    describe: "Content ID to set for the catalog item",
+                    type: "string",
+                  })
+                  .option("content_id_type", {
+                    alias: "t",
+                    describe: "Type of Content ID",
+                    type: "string",
+                  })
+                  .option("public", {
+                    alias: "p",
+                    describe: "Set Item to Public",
+                    type: "boolean",
+                  })
+                  .option("composition_key", {
+                    alias: "ck",
+                    describe: "Name of Composition Key",
+                    type: "string"
+                  });
+              },
+              (argv) => {
+                CatalogCommand.CmdCatalogItemSet({argv});
+              }        
             )
-    },
+            .command(
+              "add <object_id> [options]",
+              "Add object as catalog item",
+              (yargs) => {
+                yargs
+                  .positional("object_id", {
+                    describe: "Catalog Object ID",
+                    type: "string",
+                  })
+                  .option("item_name", {
+                    alias: "n", 
+                    describe: "New Media Item Name",
+                    type: "string"
+                  })
+                  .option("content_id", {
+                    alias: "c",
+                    describe: "Content ID to add as catalog item",
+                    type: "string",
+                  })
+                  .option("content_id_type", {
+                    alias: "t",
+                    describe: "Type of Content ID",
+                    type: "string",
+                  })
+                  .option("public", {
+                    alias: "p",
+                    describe: "Set Item to Public",
+                    type: "boolean",
+                    default: false
+                  });
+              },
+              (argv) => {
+                CatalogCommand.CmdCatalogItemAdd({argv});
+              }        
+            );
+        }        
+      );
+  },
 };
--- utilities/elv_media_wallet/lib/Catalog.js
+++ utilities/elv_media_wallet/lib/Catalog.js
@@ -3,108 +3,108 @@ const { Config } = require("../../../src/Config");
 const yaml = require("js-yaml");
 
 const CmdCatalogList = async({argv}) => {
-    console.log("Catalog List Command Invoked\n");
-
-    try {
-        let elvMediaWallet = new ElvMediaWallet({
-            configUrl: Config.networks[Config.net],
-            debugLogging: argv.verbose
-        });
-
-        await elvMediaWallet.Init({
-            privateKey: process.env.PRIVATE_KEY,
-        });
-        let res = await elvMediaWallet.CatalogList({
-            objectId: argv.object_id
-        });
+  console.log("Catalog List Command Invoked\n");
+    
+  try {
+    let elvMediaWallet = new ElvMediaWallet({
+      configUrl: Config.networks[Config.net],
+      debugLogging: argv.verbose
+    });
+    
+    await elvMediaWallet.Init({
+      privateKey: process.env.PRIVATE_KEY,
+    });
+    let res = await elvMediaWallet.CatalogList({
+      objectId: argv.object_id
+    });
         
-        console.log(yaml.dump(res));
-    } catch(error) {
-        console.error("Error listing catalog items:", error);
-    }
+    console.log(yaml.dump(res));
+  } catch (error) {
+    console.error("Error listing catalog items:", error);
+  }
 };
 
 const CmdCatalogItemGet = async({argv}) => {
-    console.log("Catalog Item Get Command Invoked\n");
+  console.log("Catalog Item Get Command Invoked\n");
 
-    try {
-        let elvMediaWallet = new ElvMediaWallet({
-            configUrl: Config.networks[Config.net],
-            debugLogging: argv.verbose
-        });
+  try {
+    let elvMediaWallet = new ElvMediaWallet({
+      configUrl: Config.networks[Config.net],
+      debugLogging: argv.verbose
+    });
 
-        await elvMediaWallet.Init({
-            privateKey: process.env.PRIVATE_KEY,
-        });
-        let res = await elvMediaWallet.CatalogItemGet({
-            objectId: argv.object_id,
-            itemId: argv.item_id,
-        });
+    await elvMediaWallet.Init({
+      privateKey: process.env.PRIVATE_KEY,
+    });
+    let res = await elvMediaWallet.CatalogItemGet({
+      objectId: argv.object_id,
+      itemId: argv.item_id,
+    });
         
-        console.log(yaml.dump(res));
-    } catch(error) {
-        console.error("Error listing catalog items:", error);
-    }
+    console.log(yaml.dump(res));
+  } catch (error) {
+    console.error("Error listing catalog items:", error);
+  }
 
-}
+};
 
 const CmdCatalogItemSet = async({argv}) => {
-    console.log("Catalog Item Set Command Invoked\n");
+  console.log("Catalog Item Set Command Invoked\n");
 
-    try {
-        let elvMediaWallet = new ElvMediaWallet({
-            configUrl: Config.networks[Config.net],
-            debugLogging: argv.verbose
-        });
+  try {
+    let elvMediaWallet = new ElvMediaWallet({
+      configUrl: Config.networks[Config.net],
+      debugLogging: argv.verbose
+    });
 
-        await elvMediaWallet.Init({
-            privateKey: process.env.PRIVATE_KEY,
-        });
-        let res = await elvMediaWallet.CatalogItemSet({
-            objectId: argv.object_id,
-            itemId: argv.item_id,
-            contentId: argv.content_id,
-            contentIdType: argv.content_id_type,
-            isPublic: argv.public, 
-            compositionKey: argv.composition_key
-        });
+    await elvMediaWallet.Init({
+      privateKey: process.env.PRIVATE_KEY,
+    });
+    let res = await elvMediaWallet.CatalogItemSet({
+      objectId: argv.object_id,
+      itemId: argv.item_id,
+      contentId: argv.content_id,
+      contentIdType: argv.content_id_type,
+      isPublic: argv.public, 
+      compositionKey: argv.composition_key
+    });
         
-        console.log(yaml.dump(res));
-    } catch(error) {
-        console.error("Error setting catalog item:", error);
-    }
+    console.log(yaml.dump(res));
+  } catch (error) {
+    console.error("Error setting catalog item:", error);
+  }
     
-}
+};
 const CmdCatalogItemAdd = async({argv}) => {
-    console.log("Catalog Item Add Command Invoked\n");
+  console.log("Catalog Item Add Command Invoked\n");
 
-    try {
-        let elvMediaWallet = new ElvMediaWallet({
-            configUrl: Config.networks[Config.net],
-            debugLogging: argv.verbose
-        });
+  try {
+    let elvMediaWallet = new ElvMediaWallet({
+      configUrl: Config.networks[Config.net],
+      debugLogging: argv.verbose
+    });
 
-        await elvMediaWallet.Init({
-            privateKey: process.env.PRIVATE_KEY,
-        });
-        let res = await elvMediaWallet.CatalogItemAdd({
-            objectId: argv.object_id,
-            itemName: argv.item_name,
-            contentId: argv.content_id,
-            contentIdType: argv.content_id_type,
-            public: argv.public
-        });
+    await elvMediaWallet.Init({
+      privateKey: process.env.PRIVATE_KEY,
+    });
+    let res = await elvMediaWallet.CatalogItemAdd({
+      objectId: argv.object_id,
+      itemName: argv.item_name,
+      contentId: argv.content_id,
+      contentIdType: argv.content_id_type,
+      public: argv.public
+    });
         
-        console.log(yaml.dump(res));
-    } catch(error) {
-        console.error("Error adding catalog item:", error);
-    }
+    console.log(yaml.dump(res));
+  } catch (error) {
+    console.error("Error adding catalog item:", error);
+  }
     
-}
+};
 
 module.exports = {
-    CmdCatalogList,
-    CmdCatalogItemGet,
-    CmdCatalogItemSet,
-    CmdCatalogItemAdd
+  CmdCatalogList,
+  CmdCatalogItemGet,
+  CmdCatalogItemSet,
+  CmdCatalogItemAdd
 };
